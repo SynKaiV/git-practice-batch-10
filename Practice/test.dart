ed0dd3129d8d590857c7416e15cd5cc01b030008
SynKaiV <adnanimtiaj201142@gmail.com>
2025-04-20 15:05:03 +0600

    Made some new changes using IDE

--- Practice/test.dart
+++ Practice/test.dart
@@ -5,4 +5,7 @@ main(){
   print("Changes have been made.");
   print("Changes have been made.");
   print("Changes have been made.");
+
+  print("Made some new changes.");
+  print('This is git learning through IDE!!!');
 }

2903a3ab533b779de50308db65964987f7138c99
SynKaiV <adnanimtiaj201142@gmail.com>
2025-04-20 15:15:04 +0600

    Feature branch (Creating conflict)

--- Practice/test.dart
+++ Practice/test.dart
@@ -1,3 +1,13 @@
 main(){
   print("Changes have been made.");
+  print("Changes have been made.");
+  print("Changes have been made.");
+  print("Changes have been made.");
+  print("Changes have been made.");
+  print("Changes have been made.");
+
+  print("Made some new changes.");
+  print('This is git learning through IDE!!!');
+
+  print('git IDE branch');
 }
